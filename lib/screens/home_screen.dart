22917db2f9c8d02afa06a643aae3e53cea45ffeb
Xeostop1 <stop1@daum.net>
2025-06-28 21:20:47 +0900

    thmbs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projets/services/api_service.dart';
 import 'package:projets/models/webtoon_model.dart';
+import 'dart:io';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -24,10 +25,10 @@ class HomeScreen extends StatelessWidget {
       ),
       body: FutureBuilder(future: webtoons, builder: (context, snapshot){
           if(snapshot.hasData){
-            return ListView(
+            return Column(
               children: [
-                for(var webtoon in snapshot.data!)
-                  Text(webtoon.title),
+                SizedBox(height: 50,),
+                Expanded(child: makeList(snapshot))
               ],
             );
           }
@@ -36,5 +37,50 @@ class HomeScreen extends StatelessWidget {
           );
       }),
     );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+            itemBuilder: (context, index){
+              var webtoon = snapshot.data![index];
+              return Column(
+                children: [
+                 Container(
+                   width: 250,
+                   clipBehavior: Clip.hardEdge,
+                   decoration: 
+                    BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 3,
+                          offset: Offset(10, 10),
+                          color: Colors.black.withOpacity(0.5)
+                        )
+                      ]
+                    ),
+                   child: Image.network(
+                     webtoon.thumb,
+                     headers: const {
+                       'Referer': 'https://comic.naver.com',
+                     },
+                   ),
+                 ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(webtoon.title,
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  )
+                ],
+              );
+            },
+            separatorBuilder: (context, index)=>SizedBox(width: 40,),
+            scrollDirection: Axis.horizontal,
+            itemCount: snapshot.data!.length,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          );
   }
 }
